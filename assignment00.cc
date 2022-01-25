d0986ec5c5c9968d60b485621674d5ee97c98cb8
Nicholas Dean <z1545207@students.niu.edu>
2022-01-25 10:19:13 -0600

    Added Doc-Box

---
+++ assignment00.cc
@@ -0,0 +1,8 @@
+/*  Nicholas Dean
+    z1545207
+    CSCI 480 OPE1
+
+    I certify that this is my own work and where appropriate an extension
+    of the starter code provided for the assignment.
+*/
+
